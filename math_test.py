c600a78cf31b2088d0ccb8d8d57dc8a84ed0085e
Kevin Koh <kevin.koh.dev@gmail.com>
2021-06-23 19:55:00 -0700

    added test_foo_2 to math_test.py

--- math_test.py
+++ math_test.py
@@ -1,21 +1,31 @@
 def foo(number1, number2):
-  result = number1 + number2
-  return result
+    result = number1 + number2
+    return result
+
 
 def test_foo():
-  number_a = 2
-  number_b = 3
-  result = foo(number_a, number_b)
-  assert result == 5
+    number_a = 2
+    number_b = 3
+    result = foo(number_a, number_b)
+    assert result == 5
 
-  number_c = 4
-  number_d = 2
-  result = foo(number_c, number_d)
-  assert result == 6
+    number_c = 4
+    number_d = 2
+    result = foo(number_c, number_d)
+    assert result == 6
 
-  print("All tests on foo finished")
+    print("All tests on foo finished")
+
+
+def test_foo_2():
+    number_a = 4
+    number_b = -2
+    result = foo(number_a, number_b)
+    assert result == 2
+
 
 if __name__ == '__main__':
-  # This calls the test_foo function 
-  # when we run from the command line
-  test_foo()  
+    # This calls the test_foo function
+    # when we run from the command line
+    test_foo()
+    test_foo_2()
